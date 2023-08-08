53c70328371cf28b013af8a6a0c335bcf5f7a206
Joykrisno <97521673+Joykrisno@users.noreply.github.com>
2023-08-08 18:30:31 +0600

    Add project files.

---
+++ MVCCURD/Controllers/Student.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCCURD.Models;
+using System;
+using System.Linq;
+
+
+namespace MVCCURD.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _Db;
+
+        public StudentController(StudentContext Db)
+        {
+            _Db = Db;
+        }
+
+        public IActionResult StudentList()
+        {
+            try
+            {
+                var stdlist = from a in _Db.tbl_Student
+                              join b in _Db.tbl_Departments
+                              on a.DepID equals b.Id
+                              into Dep
+                              from b in Dep.DefaultIfEmpty()
+                              select new Student
+                              {
+                                  Id = a.Id,
+                                  Name = a.Name,
+                                  Fname = a.Fname,
+                                  Mobile = a.Mobile,
+                                  Email = a.Email,
+                                  Description = a.Description,
+                                  DepID = a.DepID,
+                                  Department = b == null ? "" : b.Departments
+                              };
+
+                return View(stdlist);
+
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here or log it
+                return View();
+            }
+        }
+    }
+}
+
+
+ 
---
+++ MVCCURD/Models/StudentContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCCURD.Models
+
+{
+    public class StudentContext:DbContext
+
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Student> tbl_Student {  get; set; }
+        public DbSet<Department> tbl_Departments { get; set; }
+    }
+}
